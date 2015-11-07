9c62e36e88879c241db9e663ed4b3d02778e42ac
Fu, Eric <ericfu88@gmail.com>
2015-11-07 08:43:17 -0800

    Remove requirements for listening on localhost

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ if (config.isDevOrTest()) {
 // Start the server
 app.set('port', process.env.PORT || config.getConfig().port);
 
-var server = app.listen(app.get('port'), "127.0.0.1", function() {
+var server = app.listen(app.get('port'), function() {
     app.locals.startup().then(function(){
       logger.info('Fitness server listening on port ' + server.address().port);
     });
